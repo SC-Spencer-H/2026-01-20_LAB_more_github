97d4c940a62c25adf31c54df1f72f8eacf3bef70
spencer.harston <spencer.harston@ENGR7KCP5S3>
2026-01-20 14:29:35 -0700

    Add project files.

---
+++ 2026-01-20_LAB_more_github/Client.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyManagement.Models
+{
+    internal class Client
+    {
+        Guid Id { get; set; }
+        string Name { get; set; }
+        string Email { get; set; }
+        DateTime CreatedOn { get; set; }
+        private bool IsActive { get; set; }
+
+        public Client(string name, string email)
+        {
+            Name = name;
+            Email = email;
+
+            Id = Guid.NewGuid();
+            CreatedOn = DateTime.Now;
+            IsActive = true;
+        }
+
+        public Client(Guid id, string name, string email, DateTime createdOn, bool isActive)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            CreatedOn = createdOn;
+            IsActive = isActive;
+        }
+
+        public string GetSummary()
+        {
+            return $"ID: {Id}\nName: {Name}\nEmail: {Email}\nCreated on: {CreatedOn}\nIs active: {IsActive}";
+        }
+
+        public void Deactivate()
+        {
+            IsActive = false;
+        }
+    }
+}
---
+++ 2026-01-20_LAB_more_github/Program.cs
@@ -0,0 +1,14 @@
+﻿using CompanyManagement.Models;
+
+public class Program
+{
+    static void Main()
+    {
+        Client client1 = new Client(name: "SnowTech Solutions", email: "info@snowtech.com");
+        Client client2 = new Client(name: "Spencer Harston", email: "spencer.harston@students.snow.edu");
+
+        Console.WriteLine(client1.GetSummary());
+        Console.WriteLine();
+        Console.WriteLine(client2.GetSummary());
+    }
+}
